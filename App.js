bcbb039d19a3e7123eed55eb55f837c679b17af4
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-14 02:24:01 -0700

    Commiting App.js

---
+++ App.js
@@ -0,0 +1,47 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+ // import React from 'react';
+ // import { View, Text, Button, StyleSheet, TextInput } from 'react-native';
+ //
+ // const facebook = () => {
+ //   return (
+ //     <View style={styles.container}>
+ //     <Text style={styles.textStyle}>facebook</Text>
+ //     <TextInput style={styles.emailStyle} placeholder="Email"></TextInput>
+ //     <TextInput style={styles.emailStyle} placeholder="password"></TextInput>
+ //     </View>
+ //     );
+ // };
+ //
+ // const styles = StyleSheet.create({
+ //   container: {
+ //     flex: 1,
+ //     backgroundColor: '#43609C',
+ //     justifyContent: 'center',
+ //     alignItems: 'center',
+ //     paddingRight: 20,
+ //     paddingLeft: 20
+ //   },
+ //   textStyle: {
+ //    fontSize: 50,
+ //    marginBottom: 60,
+ //    color: '#fff',
+ //    fontWeight: 'bold'
+ //   },
+ //   emailStyle: {
+ //     backgroundColor: '#fff',
+ //     width: '100%',
+ //     paddingTop: 15,
+ //     paddingBottom: 15,
+ //     paddingLeft: 10,
+ //     marginTop: 20,
+ //   }
+ //   })
+ //
+ // export default facebook;
